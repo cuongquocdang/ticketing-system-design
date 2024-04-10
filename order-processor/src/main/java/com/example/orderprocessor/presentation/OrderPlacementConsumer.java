6b647b4b944b0cd8a6ee74bb8a78144768a09e96
Cuong Quoc Dang <84326038+cuongquocdang@users.noreply.github.com>
2024-04-10 21:43:53 +0700

    chore(order-processor): init prj

---
+++ order-processor/src/main/java/com/example/orderprocessor/presentation/OrderPlacementConsumer.java
@@ -0,0 +1,26 @@
+package com.example.orderprocessor.presentation;
+
+import com.example.orderprocessor.presentation.dtos.OrderPlacementRequestDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OrderPlacementConsumer {
+
+    private final ObjectMapper objectMapper;
+
+    @KafkaListener(topics = "ORDERS", groupId = "order-processor")
+    @SneakyThrows
+    public void flightEventConsumer(final String message) {
+
+        var obj = objectMapper.readValue(message, OrderPlacementRequestDTO.class);
+
+        log.info("Consumer consume Kafka message -> {}", obj);
+    }
+}
---
+++ order-processor/src/main/java/com/example/orderprocessor/presentation/dtos/OrderPlacementRequestDTO.java
@@ -0,0 +1,4 @@
+package com.example.orderprocessor.presentation.dtos;
+
+public record OrderPlacementRequestDTO(String productId) {
+}
